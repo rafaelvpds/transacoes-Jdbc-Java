8cf3b6bb150093f5733cc1e92664e1c52955cd6a
rafaelvpds <33781893+rafaelvpds@users.noreply.github.com>
2023-07-01 14:56:40 -0300

    jdbc-transação

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,32 +1,50 @@
 package application;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.Db;
-import db.DbIntegrityExeption;
+import db.DbException;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Connection conn = null;
 
-		PreparedStatement st = null;
+		Statement st = null;
+//manter a consistencia do banco de dados
 
+		// setAutoCommit = cada operação isolada que voce fizer ela vai ser confirmada
+		// automaticamente
+		// commit => confirmar a transação
+		// rollback desfazer o que foi feito ate o momento;
 		try {
 			conn = Db.getConnection();
+			conn.setAutoCommit(false); // Não e para confirma a transação automaticamente
+			st = conn.createStatement();
 
-			
+			int rows1 = st.executeUpdate("UPDATE " + "seller " + "SET BaseSalary = 2090 " + "WHERE DepartmentId = 1");
+			int x = 4;
 
+			if (x < 2) {
+				throw new SQLException("Fake Error");
+			}
+			int rows2 = st.executeUpdate("UPDATE " + "seller " + "SET BaseSalary = 3090 " + "WHERE DepartmentId = 2");
 
-			int rowAffect = st.executeUpdate();
-
-			System.out.println("Done! Rows affect: " + rowAffect);
+			conn.commit(); // vai confirmar as transações
+			System.out.println("rows1 " + rows1);
+			System.out.println("rows1 " + rows2);
 
 		} catch (SQLException e) {
-			throw new DbIntegrityExeption(e.getMessage());
-			// TODO: handle exception
+			try {
+				conn.rollback();
+				throw new DbException("Transaction rolled back! Caused by: " + e.getMessage());
+			} catch (SQLException e1) {
+
+				throw new DbException("Error trying to rollback! Caused by: " + e1.getMessage());
+			}
+
 		} finally {
 			Db.closeStatement(st);
 			Db.closeConnection();
